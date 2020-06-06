51a8b0843ade6ba8091417220c2c8c9684031e4a
Willian Marciano <willian_marciano@hotmail.com>
2020-06-06 00:35:16 -0300

    Acrescentando break e continue

--- Jogos/advinhacao.py
+++ Jogos/advinhacao.py
@@ -8,14 +8,19 @@ numero_secreto = 42
 for rodada in range(1,total_de_tentativas + 1):
 
     print("Tentativa {} de {}".format(rodada,total_de_tentativas))
-    chute = int(input("Digite o seu numero: "))
+    chute = int(input("Digite um número entre 1 e 100: "))
     print("você digitou ", chute)
+
+    if chute < 0 or chute > 100:
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
     acertou = chute == numero_secreto
     maior   = chute > numero_secreto
 
     if acertou:
         print("você acertou o número secreto")
+        break
     else:
         if maior:
             print("Você errou! O Seu chute foi maior do que o número secreto.")
