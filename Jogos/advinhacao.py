01c6def8727dfec323f5c711a29b81d327f1a039
Willian Marciano <willian_marciano@hotmail.com>
2020-06-05 23:12:39 -0300

    Aula 2 - Ajustando o programa

--- Jogos/advinhacao.py
+++ Jogos/advinhacao.py
@@ -1,1 +1,14 @@
-print("Hello aqui no Python usando PyCharm")
+print("********************************")
+print("Bem Vindo ao jogo de Adivinhação")
+print("********************************")
+
+numero_secreto = 42
+
+chute = int(input("Digite o seu numero: "))
+
+print("você digitou ", chute)
+
+if numero_secreto == chute:
+    print("você acertou o numero secreto")
+else:
+    print("Você errou")
