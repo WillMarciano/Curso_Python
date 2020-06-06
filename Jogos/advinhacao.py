c4259bf351202e7524e0132776662588c59abf43
Willian Marciano <willian_marciano@hotmail.com>
2020-06-05 23:48:13 -0300

    Incluindo 3 tentativas de chute

--- Jogos/advinhacao.py
+++ Jogos/advinhacao.py
@@ -3,12 +3,23 @@ print("Bem Vindo ao jogo de Adivinhação")
 print("********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute = int(input("Digite o seu numero: "))
+while (rodada <= total_de_tentativas):
+    print("Tentativa ", rodada , " do total de ", total_de_tentativas)
+    chute = int(input("Digite o seu numero: "))
+    print("você digitou ", chute)
 
-print("você digitou ", chute)
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
 
-if numero_secreto == chute:
-    print("você acertou o numero secreto")
-else:
-    print("Você errou")
+    if acertou:
+        print("você acertou o número secreto")
+    else:
+        if maior:
+            print("Você errou! O Seu chute foi maior do que o número secreto.")
+        else:
+            print("Você errou! O Seu chute foi menor do que o número secreto.")
+    rodada = rodada + 1
+print("Fim do Jogo")
