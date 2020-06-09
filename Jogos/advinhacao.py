d58d6b7c7e370d409b4bf163b17aad8413f393fd
Willian Marciano <willian_marciano@hotmail.com>
2020-06-08 21:12:00 -0300

    Setando Níveis de dificuldade

--- Jogos/advinhacao.py
+++ Jogos/advinhacao.py
@@ -4,8 +4,20 @@ print("********************************")
 print("Bem Vindo ao jogo de Adivinhação")
 print("********************************")
 
-total_de_tentativas = 3
+total_de_tentativas = 0
 numero_secreto = round(random.randrange(1,101))
+
+print("Informe o Nível de Dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Defina o Nível:"))
+
+if nivel == 1:
+    total_de_tentativas = 20
+elif nivel == 2:
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1,total_de_tentativas + 1):
 
