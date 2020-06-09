be2cebf050eeada63d9542e652b5d697f3e2a161
Willian Marciano <willian_marciano@hotmail.com>
2020-06-08 21:04:05 -0300

    Adicionando Níveis jogo

--- Jogos/advinhacao.py
+++ Jogos/advinhacao.py
@@ -5,8 +5,20 @@ print("Bem Vindo ao jogo de Adivinhação")
 print("********************************")
 
 numero_secreto = round(random.randrange(1,101))
-total_de_tentativas = 3
+total_de_tentativas = 0
 rodada = 1
+
+print("Informe o Nível de Dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Defina o nível:"))
+
+if (nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2 ):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas=5
 
 while (rodada <= total_de_tentativas):
     print("Tentativa ", rodada , " do total de ", total_de_tentativas)
--- Jogos/venv/Scripts/activate.bat
+++ Jogos/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Alura\Python\Projetos\Jogos\venv
+set VIRTUAL_ENV=Y:\Alura\Python\Projetos\Jogos\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
