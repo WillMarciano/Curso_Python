d8437572b30dda6212289845fd1a044126d53a43
Willian Marciano <willian_marciano@hotmail.com>
2020-06-10 02:00:29 -0300

    Jogo da Forca

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -1,10 +1,37 @@
-
 def jogar():
     print("********************************")
     print("***Bem Vindo ao jogo da Forca***")
     print("********************************")
 
+    palavra_secreta = "laranja".upper()
+    acertou = False
+    enforcou = False
+    palavra = [" "] * len(palavra_secreta)
+    letras_utilizadas = ""
+    chances = 5
+
+    while (not acertou and not enforcou):
+        print(palavra)
+        print()
+        print("Chances = {} - Letras Digitadas: {}".format(chances,letras_utilizadas))
+        print()
+        chute = input("Qual a Letra? ").upper()
+
+        i = 0
+        for letra in palavra_secreta:
+            if i < len(palavra):
+                if chute == letra:
+                    palavra[i] = chute
+
+            i = i + 1
+        letras_utilizadas = letras_utilizadas + chute + ', '
+
+        print()
+        print("Jogando...")
+        print()
+
     print("Fim do Jogo")
 
-if (__name__ == "__main__"):
+
+if __name__ == "__main__":
     jogar()
