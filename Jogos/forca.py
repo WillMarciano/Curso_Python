998ad9206cad437e472f36aa5910a62013383bba
Willian Marciano <willian_marciano@hotmail.com>
2020-06-14 14:05:31 -0300

    Inlcuido validação de acertos e erros para finalizar o jogo

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -4,28 +4,41 @@ def jogar():
     print("********************************")
 
     palavra_secreta = "laranja".upper()
+    palavra = ["_"] * len(palavra_secreta)
+
     acertou = False
     enforcou = False
-    palavra = ["_"] * len(palavra_secreta)
+
     letras_utilizadas = ""
-    chances = 5
+    chances = 6
+    tentativa = 0
 
     while (not acertou and not enforcou):
         print(palavra)
         print("Chances = {} - Letras Digitadas: {}".format(chances,letras_utilizadas))
-        chute = input("Qual a Letra? ").upper().strip()
+        chute = input("Qual a Letra? ").strip().upper()
 
         i = 0
-        for letra in palavra_secreta:
-            if i < len(palavra):
+        if chute in palavra_secreta:
+            for letra in palavra_secreta:
                 if chute == letra:
                     palavra[i] = chute
+                i += 1
+                tentativa += 1
+        else:
+            chances -= 1
 
-            i = i + 1
+        enforcou = chances == 0
+        acertou = "_" not in palavra
+
         letras_utilizadas = letras_utilizadas + chute + ', '
         print("Jogando...")
+        print("")
 
-
+    if acertou:
+        print("Você Venceu")
+    else:
+        print("Você Perdeu")
     print("Fim do Jogo")
 
 
